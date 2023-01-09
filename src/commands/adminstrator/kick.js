b744c3a525b68f759081c5c2c9c9f04b41527513
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2023-01-09 12:10:33 +0300

    delete unimportant file

--- src/commands/adminstrator/kick.js
+++ src/commands/adminstrator/kick.js
@@ -2,13 +2,12 @@ export default {
     name: "at",
     permission: "KickMembers",
     execute(message , args){
-        if (!args) {
-            message.reply('atılacak kişinin id sini yazın')
-            return
-        }
+        
         console.log(args[0]);
         message.guild.members.kick(args.toString())
         .then(message.reply('kullanıcı atıldı'))
-        
+        .then((msg) => {
+            setTimeout(() => msg.delete(), 5000);
+          })
     }
 }
